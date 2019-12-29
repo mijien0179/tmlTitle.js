ecd6545f8804af23eac4ea350bfadc93171e81c5
Min <mijien0179@gmail.com>
2019-12-29 23:20:17 +0900

    v19.12.29. release

---
+++ indexor.md
@@ -0,0 +1,93 @@
+# tmlTitle Indexor
+---
+기존 tmlTitle에서 추가된 기능입니다. 문서 내용 기반으로 하여 간단한 목차를 생성합니다.
+
+### INDEX
+* [구성 속성](#구성-속성)
+* [스크립트 적용 방법](#스크립트-적용-방법)
+* [이용 방법](#이용-방법)
+
+### 구성 속성
+이 기능의 구성 속성은 다음과 같습니다. **굵게 표시된 속성은 필수 값입니다.**
+
+|속성 이름                  |데이터 타입            |기본값     |설명   							|
+|--------------------------:|:----------------------|:---------:|-----------------------------------|
+|**contentQuery**			|Array(String)			|-			|글의 시작을 담고 있는 태그 쿼리	|
+|trigger					|String                 |'# index'	|글의 목차를 만들지 결정하는 트리거	|
+|indexorTitleTag			|String					|'h3'		|목차 제목의 태그					|
+|indexorTitle				|String					|'Index'	|목차 제목							|
+|orderIndex					|Boolean				|'false'	|목차 스타일(true:번호)				|
+
+### 스크립트 적용 방법
+스크립트 적용은 [이 문서를 확인](readme.md#스크립트-적용-방법)하세요. 이 기능을 이용하기 위한 코드는 다음과 같습니다.
+```
+window.addEventListener('load', function () {
+    tmlTitle({
+        tagIndexor:{
+            contentQuery:['div.area_view']
+        }
+    });
+});
+```
+
+> ### 질문1
+> [moreLess](readme.md)기능과 함께 사용하고싶어요.
+>
+> &nbsp; 이럴 땐 아래처럼 작성하면 함께 이용할 수 있습니다.
+```
+<script>
+    window.addEventListener('load', function(){
+        tmlTitle({
+            moreLessChanger:{
+                delTitleContent:true,
+                addButton:true
+            }, 
+            tagIndexor:{
+                contentQuery:["div.area_view > div", "div.area_view"]
+            }
+        });
+        
+    });
+</script>
+```
+위와 같이 해당 기능을 이용하기 위한 속성을 **반점(컴마)로 구분하여 작성**해주면 됩니다.
+
+여기에서 contentQuery에 들어갈 문자열을 찾는 방법은 다음과 같습니다.
+![쿼리 찾기](./images/indexor-findquery.png)
+포스트에서 **F12**를 눌러 개발자 도구를 엽니다.
+
+![쿼리 찾기2](./images/indexor-findquery2.png)
+![쿼리 찾기3](./images/indexor-findquery3.png)
+상단 버튼을 클릭, 글 영역을 선택합니다.
+
+![쿼리 찾기4](./images/indexor-findquery4.png)
+오른쪽 소스코드에서 본문의 코드에 마우스를 가져다 대었을 때 본문의 단락이 파랗게 표시되면 찾은 것입니다.
+
+![쿼리 찾기5](./images/indexor-findquery5.png)
+이제 찾은 값을 가지고 본문을 감싸는 태그를 CSS 선택자 문자열로 바꿉니다.
+여기에서 구조는
+```
+    <div class="area_view" ...>
+        <div class="tt...">
+    .
+    .
+    .
+```
+이므로, 선택자 문자열은 다음과 같습니다.
+> div.area_view > div
+
+CSS 선택자에 대한 내용은 [다음 포스트](https://code.tutsplus.com/ko/tutorials/the-30-css-selectors-you-must-memorize--net-16048)를 참고하면 좋습니다.
+
+> ### 질문2
+> 어째서 contentQuery 속성은 일반 문자열이 아니라 배열인가요?
+>
+> &nbsp; 원래는 일반 문자열로 구조를 작성하였으나 일부 경우에 본문 구조가 달라 스크립트 오류가 발샘함을 알게 되었습니다. 이를 보완하기 위해 배열 문자열로 처리하고, **단락 태그 *p*가 가장 많은 구역을 본문으로 인식하여 목차를 처리**하도록 변경하였습니다.
+> &nbsp; 따라서 일부 문서에서 오류가 발생하거나, 목차 생성이 되지 않는다면 해당 포스트의 구조를 살펴보세요. 기존 지정해놓은 구조와 다르다면 배열로서 옆에 추가하면 오류를 해결할 수 있습니다.
+
+### 이용 방법
+
+![적용 방법](./images/indexor-applying.png)
+글의 마지막에 **trigger 속성으로 준 문자열을 작성**해주면 됩니다.
+
+![적용 예](./images/indexor-result.png)
+적용 예 : https://pang2h.tistory.com/260
--- tmlTitle.js
+++ tmlTitle.js
@@ -8,9 +8,9 @@
 function tmlTitle(data) {
 
     let scriptInfo = {
-        author:`Min`,
-        blog:`https://pang2h.tistory.com?tmlTitle`,
-        git:`https://github.com/mijien0179/tmlTitle.js`
+        author: `Min`,
+        blog: `https://pang2h.tistory.com?tmlTitle`,
+        git: `https://github.com/mijien0179/tmlTitle.js`
     }
 
     function moreLessChanger(data) {
@@ -19,26 +19,26 @@ function tmlTitle(data) {
         function close(parent, title) {
             parent.classList.remove('open');
             let btn = parent.querySelectorAll(`a.btn-toggle-moreless`);
-            for(let i = 0; i < btn.length;++i){
+            for (let i = 0; i < btn.length; ++i) {
                 btn[i].innerText = title;
             }
-            if(btn[1]) btn[1].style.display='none';
+            if (btn[1]) btn[1].style.display = 'none';
         }
-        
+
         function open(parent, title) {
             parent.classList.add('open');
             let btn = parent.querySelectorAll(`a.btn-toggle-moreless`);
-            for(let i = 0; i < btn.length;++i){
+            for (let i = 0; i < btn.length; ++i) {
                 btn[i].innerText = title;
             }
-            if(btn[1]) btn[1].style.display=null;
+            if (btn[1]) btn[1].style.display = null;
         }
 
         for (let i = 0; i < parent.length; ++i) {
             if (data.addButton === true) {
                 parent[i].innerHTML += parent[i].querySelector('a.btn-toggle-moreless').outerHTML;
                 parent[i].lastChild.classList.add('tmlTitle-extrabtn');
-                parent[i].lastChild.style.display=`none`;                
+                parent[i].lastChild.style.display = `none`;
             }
             let visBtn = parent[i].querySelectorAll('a.btn-toggle-moreless');
             let content = parent[i].querySelectorAll('div.moreless-content *');
@@ -79,13 +79,26 @@ function tmlTitle(data) {
         }
     }
 
-    function tagIndexor(data){
+    function tagIndexor(data) {
         {   // indexor creatable
-            if(document.body.id != 'tt-body-page') return;
-            let pDoc = document.querySelectorAll(`${data.contentQuery} > p`);
+            if (document.body.id != 'tt-body-page') return;
+            let pDoc;
+            let tpDoc = {
+                query: data.contentQuery[0],
+                size: document.querySelectorAll(`${data.contentQuery[0]} > p`).length
+            };
+            for (let i = 1; i < data.contentQuery.length; ++i) {
+                pDoc = document.querySelectorAll(`${data.contentQuery[i]} > p`);
+                if (tpDoc.size < pDoc.length){
+                    tpDoc.query = data.contentQuery[i];
+                    tpDoc.size = pDoc.length;
+                }
+            }
+            data.contentQuery = tpDoc.query;
+            pDoc = document.querySelectorAll(`${data.contentQuery} > p`);
             data.trigger = data.trigger || '# index';
-            if(pDoc[pDoc.length-1].innerText.toLowerCase() != data.trigger) return;
-            else pDoc[pDoc.length-1].remove();
+            if (pDoc[pDoc.length - 1].innerHTML.toLowerCase() != data.trigger) return;
+            else pDoc[pDoc.length - 1].remove();
         }
 
         let curTag = document.querySelector(`${data.contentQuery} > hr`);
@@ -93,30 +106,30 @@ function tmlTitle(data) {
         data.indexorTitleTag = data.indexorTitleTag || 'h3';
         data.indexorTitle = data.indexorTitle || 'Index';
         let orderIndexor = {
-            open: data.orderIndex && `<ol>`  || `<ul>`,
+            open: data.orderIndex && `<ol>` || `<ul>`,
             close: data.orderIndex && `</ol>` || `</ul>`
         };
         let nod = [];
-        while(curTag){
+        while (curTag) {
             let curTagName = curTag.tagName.toLowerCase();
-            if(indexorTagList.contains(curTagName)){
+            if (indexorTagList.contains(curTagName)) {
                 let idValue = `tmlTitle-tagIndexor-${nod.length}`;
-                if(curTag.id == ''){
+                if (curTag.id == '') {
                     curTag.id = idValue;
-                }else{
+                } else {
                     idValue = curTag.id;
                 }
                 nod.push({
-                    id:idValue,
-                    text:curTag.innerText
+                    id: idValue,
+                    text: curTag.innerHTML
                 });
             }
             curTag = curTag.nextElementSibling;
         }
-        if(nod == []) return;
+        if (nod == []) return;
         let ret = `<div id="tmlTitle-tagIndexor"><${data.indexorTitleTag}>${data.indexorTitle}</${data.indexorTitleTag}>`;
         ret += `${orderIndexor.open}`
-        for(let i = 0; i < nod.length;++i){
+        for (let i = 0; i < nod.length; ++i) {
             ret += `<li><a href="#${nod[i].id}">${nod[i].text}</a></li>`;
         }
         ret += `${orderIndexor.close}</div>`;
@@ -126,18 +139,18 @@ function tmlTitle(data) {
 
     }
 
-    if(data.moreLessChanger){
+    if (data.moreLessChanger) {
         moreLessChanger(data.moreLessChanger);
     }
 
-    if(data.tagIndexor){
+    if (data.tagIndexor) {
         tagIndexor(data.tagIndexor);
     }
 
     console.log(`tmlTitle.js : 티스토리 블로그 커스텀 스크립트\n` +
-                `개발자블로그 : ${scriptInfo.blog}\n` +
-                `Git 주소: ${scriptInfo.git}\n\n` +
-                `이용할 경우 이 로그를 포함한 이 로그에서 사용하는 정보를 변경하거나 삭제를 제한합니다.\n`+
-                `단, 코드 변경자에 한하여 기존 로그를 유지한 채 정보를 추가하는 것은 허용합니다.`);
+        `개발자블로그 : ${scriptInfo.blog}\n` +
+        `Git 주소: ${scriptInfo.git}\n\n` +
+        `이용할 경우 이 로그를 포함한 이 로그에서 사용하는 정보를 변경하거나 삭제를 제한합니다.\n` +
+        `단, 코드 변경자에 한하여 기존 로그를 유지한 채 정보를 추가하는 것은 허용합니다.`);
 
 }
